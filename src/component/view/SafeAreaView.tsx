523bd68674f5ad9051c096552aec442f2282b808
TrungDX_Max <trung13988@gmail.com>
2023-08-27 21:42:45 +0700

    feat

--- src/component/view/SafeAreaView.tsx
+++ src/component/view/SafeAreaView.tsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { SafeAreaView as SafeAreaViewRN, ViewProps } from "react-native";
-import { ThemeProps } from "../../interface/iTheme";
-import { getStyleWithTheme } from "../../style/style";
+import React from 'react';
+import {SafeAreaView as SafeAreaViewRN, ViewProps} from 'react-native';
+import {ThemeProps} from '../../interface/iTheme';
+import {getStyleWithTheme} from '../../style/style';
 
 export interface ISafeAreaViewProps extends ViewProps, ThemeProps {
-  className?: string;
-  children?: any;
+    className?: string;
+    children?: any;
 }
 
 const SafeAreaView: React.FC<ISafeAreaViewProps> = ({
-  children,
-  style,
-  useLightColor,
-  colorDarkMode,
-  ...rest
-}) => {
-  const listStyle = getStyleWithTheme(
-    rest,
+    children,
     style,
+    useLightColor,
     colorDarkMode,
-    useLightColor
-  );
-  return <SafeAreaViewRN style={listStyle}>{children}</SafeAreaViewRN>;
+    ...rest
+}) => {
+    const listStyle = getStyleWithTheme(rest, style, {
+        colorDarkMode,
+        useLightColor,
+    });
+    return <SafeAreaViewRN style={listStyle}>{children}</SafeAreaViewRN>;
 };
 
 export default SafeAreaView;
