957f545bacfed496fd998cb7dad6a862eeb24bb1
do_xuan_trung_iMac <trung13988@gmail.com>
2023-10-25 16:31:58 +0700

    v0.1.58

--- src/component/image/ImagePreview.tsx
+++ src/component/image/ImagePreview.tsx
@@ -1,62 +1,62 @@
-import React, { useState } from "react";
-import ClassNames from "classnames";
-import TouchableOpacity from "../view/TouchableOpacity";
-import View from "../view/View";
-import Image, { IImageProps } from "./Image";
-import ImageViewerModal from "./ImageViewerModal";
-import { IAvatarProps } from "../avatar/Avatar";
+import React, {useState} from 'react';
+import ClassNames from 'classnames';
+import TouchableOpacity from '../view/TouchableOpacity';
+import View from '../view/View';
+import Image, {IImageProps} from './Image';
+import ImageViewerModal from './ImageViewerModal';
+import {IAvatarProps} from '../avatar/Avatar';
+import {styleTransformer} from '../../style/style';
 
 export interface IImagePreviewProps extends Partial<IImageProps> {
-  showLightBox?: boolean;
-  onPress?: (props?: any) => any;
-  uri?: string;
-  size?: IAvatarProps["size"];
-  classNameContainer?: string;
+    showLightBox?: boolean;
+    onPress?: (props?: any) => any;
+    uri?: string;
+    size?: IAvatarProps['size'];
+    classNameContainer?: string;
 }
 
 const ImagePreview: React.FC<IImagePreviewProps> = ({
-  showLightBox,
-  onPress,
-  source,
-  uri,
-  size = "medium",
-  className,
-  classNameContainer,
-  ...rest
+    showLightBox,
+    onPress,
+    source,
+    uri,
+    size = 'medium',
+    className,
+    classNameContainer,
+    ...rest
 }) => {
-  const [openLightBox, setOpenLightBox] = useState(false);
-  const imageClass = ClassNames(
-    `my-2 image-square-x-large image-square-${size}`,
-    className
-  );
+    const [openLightBox, setOpenLightBox] = useState(false);
+    const imageClass = styleTransformer(
+        `my-2 image-square-x-large image-square-${size}`,
+        className,
+    );
 
-  return (
-    <View className={classNameContainer}>
-      <TouchableOpacity
-        onPress={() => {
-          if (onPress) {
-            return onPress();
-          }
-          if (showLightBox) {
-            return setOpenLightBox(true);
-          }
-        }}
-      >
-        <Image
-          {...rest}
-          source={uri ? { uri } : (source as any)}
-          className={imageClass}
-        />
-      </TouchableOpacity>
-      {openLightBox && (
-        <ImageViewerModal
-          open={openLightBox}
-          onClose={() => setOpenLightBox(false)}
-          value={[{ url: source } as any]}
-        />
-      )}
-    </View>
-  );
+    return (
+        <View className={classNameContainer}>
+            <TouchableOpacity
+                onPress={() => {
+                    if (onPress) {
+                        return onPress();
+                    }
+                    if (showLightBox) {
+                        return setOpenLightBox(true);
+                    }
+                }}>
+                <Image
+                    {...rest}
+                    source={uri ? {uri} : (source as any)}
+                    style={[imageClass]}
+                />
+            </TouchableOpacity>
+            {openLightBox && (
+                <ImageViewerModal
+                    open={openLightBox}
+                    onClose={() => setOpenLightBox(false)}
+                    value={[{url: source} as any]}
+                />
+            )}
+        </View>
+    );
 };
 
 export default ImagePreview;
